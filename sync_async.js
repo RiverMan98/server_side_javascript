5e243563ca19b272795aeb8dc966fe2b1379a043
RiverMan98 <kangin0302@gmail.com>
2022-03-13 00:11:05 +0900

    practice_sync-async

---
+++ sync_async.js
@@ -0,0 +1,11 @@
+let fs=require('fs');
+console.log(1);
+let data = fs.readFileSync('data.txt',{encoding:'utf8'});
+console.log(data);
+
+console.log(2);
+fs.readFile('data.txt', {encoding:'utf8'},function(err,data){
+    console.log(3);
+    console.log(data);
+})
+console.log(4);
