f424f7e256c8255e66fdaae6d17ddb37adcb2fc0
RiverMan98 <kangin0302@gmail.com>
2022-03-13 13:11:31 +0900

    practice_module file

---
+++ module.js
@@ -0,0 +1,5 @@
+var o = require('os');
+console.log(o.platform());
+
+// require('module') 로 사용
+//console.함수(메소드) 로 모듈 안에 있는 함수 사용
--- uglified.js
+++ uglified.js
@@ -1,6 +1,7 @@
 function hello(l){console.log("Hi,"+l)}hello("riverman");
 
-// npm install uglify-js -g --> 설치방법 참조
-//>uglifyjs pretty.js -o uglified.js  -m
-// -o : 실행한 결과를 다른 파일로 저장
-// -m : 변수까지 간단하게 바꿔줌
+/* npm install uglify-js -g --> 설치방법 참조
+-->uglifyjs pretty.js -o uglified.js  -m
+ -o : 실행한 결과를 다른 파일로 저장
+ -m : 변수까지 간단하게 바꿔줌
+ */
